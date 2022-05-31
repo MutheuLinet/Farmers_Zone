f6d6af4cae4279937327651424900f1f516ed902
MutheuLinet <muneenilinet@gmail.com>
2022-05-31 11:35:23 +0300

    Add drawables images and fonts

---
+++ app/src/main/java/com/farmersgroup/validator/Validator.java
@@ -0,0 +1,15 @@
+//package com.moringaschool.validator;
+//
+//public class Validator {
+//
+//    public boolean isValidName(String name){
+//        if (name.length()<3){
+//
+//            return false;
+//
+//        }
+//
+//        return true;
+//
+//    }
+//}
