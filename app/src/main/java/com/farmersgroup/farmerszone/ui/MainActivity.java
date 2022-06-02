b279e1d37c66d340c54723d5136ec1a961d2e660
MutheuLinet <muneenilinet@gmail.com>
2022-06-02 10:58:18 +0300

    Rename files

--- app/src/main/java/com/farmersgroup/farmerszone/ui/MainActivity.java
+++ app/src/main/java/com/farmersgroup/farmerszone/ui/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, ActionsActivity.class);
                 intent.putExtra("name", name);
                 startActivity(intent);
+                Log.d(TAG, "onClick: btnclick");
             }
         });
     }
