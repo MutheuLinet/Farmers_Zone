2a5b8a2730a5da153b039ae64a2d76499b1119ad
MutheuLinet <muneenilinet@gmail.com>
2022-06-14 09:16:24 +0300

    Add User model

--- app/src/main/java/com/farmersgroup/farmerszone/models/User.java
+++ app/src/main/java/com/farmersgroup/farmerszone/models/User.java
@@ -1,2 +1,91 @@
-package com.farmersgroup.farmerszone.models;public class User {
+package com.farmersgroup.farmerszone.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String imageURL;
+    private String phoneNumber;
+    private String status;
+    private String search;
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public User() {
+
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
 }
