737e159d4b6ce1a9e6f1ef41d4a5ba26a467ec31
MutheuLinet <muneenilinet@gmail.com>
2022-06-14 20:22:34 +0300

    Edit about and account layout files

---
+++ app/src/main/java/com/farmersgroup/farmerszone/ui/MyUserFragment.java
@@ -0,0 +1,72 @@
+package com.farmersgroup.farmerszone.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.farmersgroup.farmerszone.R;
+
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
+import android.view.View;
+
+import com.farmersgroup.farmerszone.adapters.UserAdapter;
+import com.farmersgroup.farmerszone.models.User;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MyUserFragment extends AppCompatActivity {
+
+    private UserAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_account);
+        FloatingActionButton buttonEdit = findViewById(R.id.button_edit);
+        buttonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyUserFragment.this, RegisterActivity.class));
+            }
+        });
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+        FirebaseRecyclerOptions<User> options =
+                new FirebaseRecyclerOptions.Builder<User>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("users"), User.class)
+                        .build();
+
+        adapter = new UserAdapter(options);
+        RecyclerView recyclerView = findViewById(R.id.recview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.deleteItem(viewHolder.getAdapterPosition());
+            }
+        }).attachToRecyclerView(recyclerView);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+}
---
+++ app/src/main/java/com/farmersgroup/farmerszone/ui/about/AboutFragment.java
@@ -0,0 +1,16 @@
+package com.farmersgroup.farmerszone.ui.about;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.farmersgroup.farmerszone.R;
+
+public class AboutFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_about);
+    }
+}
