26e8fbcc896d116f1b03a2f3f09951c9c6143051
MutheuLinet <muneenilinet@gmail.com>
2022-06-14 09:22:09 +0300

    Add firebase details

--- app/src/main/java/com/farmersgroup/farmerszone/ui/BrowseAllActivity.java
+++ app/src/main/java/com/farmersgroup/farmerszone/ui/BrowseAllActivity.java
@@ -1,11 +1,13 @@
 package com.farmersgroup.farmerszone.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 
 import com.farmersgroup.farmerszone.R;
@@ -15,6 +17,7 @@ import com.farmersgroup.farmerszone.models.ResultById;
 import com.farmersgroup.farmerszone.network.Api;
 import com.farmersgroup.farmerszone.network.ApiClient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -27,13 +30,37 @@ public class BrowseAllActivity extends AppCompatActivity {
 
     public static final String TAG = BrowseAllActivity.class.getSimpleName();
 
+    public List<ResultById>allItemsList;
+
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
+
     @BindView(R.id.resultsRecycleView) RecyclerView mResultsRecycleView;
+    @BindView(R.id.searchView)
+    androidx.appcompat.widget.SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_browse_all_two);
+        setContentView(R.layout.activity_browse_all);
         ButterKnife.bind(this);
+
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                String query = searchView.getQuery().toString().trim();
+                filterList(query);
+                return true;
+            }
+        });
+
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         Api client = ApiClient.getClient();
         Call<BrowseAllResponse> call = client.getAllItems("all"); //search=all
@@ -44,7 +71,7 @@ public class BrowseAllActivity extends AppCompatActivity {
                 Log.d(TAG, "onResponse: resp");
                 if (response.isSuccessful()){
 
-                    List<ResultById> allItemsList = response.body().getResults();
+                    allItemsList = response.body().getResults();
 
                     ResultsRecViewAdapter adapter = new ResultsRecViewAdapter(allItemsList, BrowseAllActivity.this);
                     adapter.setResultList(allItemsList);
@@ -58,6 +85,25 @@ public class BrowseAllActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void filterList(String text) {
+        List<ResultById> filteredList = new ArrayList<>();
+
+        for (ResultById result : allItemsList){
+            if (result.getTfvname().toLowerCase().contains(text.toLowerCase())) {
+                filteredList.add(result);
+            }
+        }
+        if (filteredList.isEmpty()){
+            Toast.makeText(this, "Item not available", Toast.LENGTH_SHORT).show();
+        }else {
+            ResultsRecViewAdapter adapter = new ResultsRecViewAdapter(allItemsList, BrowseAllActivity.this);
+            adapter.setFilteredResultList(filteredList);
+            mResultsRecycleView.setAdapter(adapter);
+            mResultsRecycleView.setLayoutManager(new LinearLayoutManager(BrowseAllActivity.this));//getApplicationContext()
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
